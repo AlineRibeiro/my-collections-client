9fb1088e961b35096ea63a833a68e136e03371f1
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-20 19:12:03 -0800

    Make an api request to snacks#index

--- src/api/Snacks.js
+++ src/api/Snacks.js
@@ -3,7 +3,6 @@ import { baseUrl } from "../config";
   export  const Snack = {
   index() {
     return fetch(`${baseUrl}/snacks`, {
-      credentials: "include"
     }).then(res => res.json());
   },
 };
--- src/features/Snacks/SnackParent.jsx
+++ src/features/Snacks/SnackParent.jsx
@@ -3,21 +3,19 @@ import React, { useState, useEffect } from "react";
 import { Snack } from '../../api/Snacks'
 
 const SnackParent = () => {
-  const [snackIndex, setSnackIndex] = useState({
-    snacks: 'testing'
-  });
-  //
-  // const fetchSnackIndex = () => {
-  //   Snack.index().then(response => {
-  //     setSnackIndex( response.snacks )
-  //   });
-  // }
-  //
-  // useEffect(() => {
-  //   fetchSnackIndex();
-  // });
+  const [snackIndex, setSnackIndex] = useState('a');
 
-  return <div>This is: { snackIndex.snacks } </div>;
+  const fetchSnackIndex = () => {
+    Snack.index().then(response => {
+      setSnackIndex( response.snacks );
+    });
+  }
+
+  useEffect(() => {
+    fetchSnackIndex();
+  });
+
+  return <div>This is: { snackIndex } </div>;
 };
 
 export default SnackParent;
