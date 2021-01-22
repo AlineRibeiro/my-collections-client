4d0832a4c2fe07e7a5e77c8617f0d3c6bf43af7b
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-21 20:13:42 -0800

    Create sign in and up forms

--- src/features/authentication/SignInForm.jsx
+++ src/features/authentication/SignInForm.jsx
@@ -1,21 +1,92 @@
 import React from "react";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import SignFormStyles from './SignFormStyles';
+
+
+export default function SignInForm() {
+  const classes = SignFormStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+         Login
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Login
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/users" variant="body2">
+                {"Don't have an account? Register now!"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
+
+
 
 // import { Snack } from "../../api/Snacks";
 
-const SignInForm = () => {
-  // const [snackIndex, setSnackIndex] = useState("");
 
-  // const fetchSnackIndex = () => {
-  //   Snack.index().then((response) => {
-  //     setSnackIndex(response.snacks);
-  //   });
-  // };
+// const [snackIndex, setSnackIndex] = useState("");
 
-  // useEffect(() => {
-  //   fetchSnackIndex();
-  // });
+// const fetchSnackIndex = () => {
+//   Snack.index().then((response) => {
+//     setSnackIndex(response.snacks);
+//   });
+// };
 
-  return <div>This will be the SignInForm </div>;
-};
-
-export default SignInForm;
+// useEffect(() => {
+//   fetchSnackIndex();
+// });
--- src/features/authentication/SignUpForm.jsx
+++ src/features/authentication/SignUpForm.jsx
@@ -1,21 +1,76 @@
 import React from "react";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
-// import { Snack } from "../../api/Snacks";
+import SignFormStyles from './SignFormStyles';
 
-const SignUpForm = () => {
-  // const [snackIndex, setSnackIndex] = useState("");
+export default function SignUpForm() {
+  const classes = SignFormStyles();
 
-  // const fetchSnackIndex = () => {
-  //   Snack.index().then((response) => {
-  //     setSnackIndex(response.snacks);
-  //   });
-  // };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Register
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="sign-in" variant="body2">
+                {"Already have an account? Sign In"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
 
-  // useEffect(() => {
-  //   fetchSnackIndex();
-  // });
-
-  return <div>This will be the SignUpForm </div>;
-};
-
-export default SignUpForm;
