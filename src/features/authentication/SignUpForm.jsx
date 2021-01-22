2f2eb99e4277354c6aa98651ed240ae2a1c8d8ca
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-22 12:30:27 -0800

    Display errors in sign up form

--- src/features/authentication/SignUpForm.jsx
+++ src/features/authentication/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -8,10 +8,48 @@ import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Alert from "@material-ui/lab/Alert";
 
 import SignFormStyles from "./SignFormStyles";
+import { User } from "../../api/User";
 
 export default function SignUpForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [messageError, setMessageError] = useState({ email: '', password: ''});
+  const [errorAlert, setErrorAlert] = useState(false);
+
+  const handleClose = () => {
+    setErrorAlert(false);
+  };
+
+  const displayError = (messageError) => {
+    const entries = Object.entries(messageError)
+    return(
+          <Alert onClose={handleClose} severity="error">
+            { entries }
+          </Alert>
+        )
+}
+
+
+  const fetchUserCreate = (event) => {
+    event.preventDefault();
+    const requestBody = {
+      user: { email, password },
+    };
+
+    User.create(requestBody).then((response) => {
+      console.log(response);
+      if (!response.errors) {
+        alert("Your user has been created");
+      } else {
+        setMessageError({ email: response.errors.email, password: response.errors.password } )
+        setErrorAlert(true);
+      }
+    });
+  };
+
   const classes = SignFormStyles();
 
   return (
@@ -24,7 +62,7 @@ export default function SignUpForm() {
         <Typography component="h1" variant="h5">
           Register
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} noValidate onSubmit={fetchUserCreate}>
           <TextField
             variant="outlined"
             margin="normal"
@@ -35,6 +73,7 @@ export default function SignUpForm() {
             name="email"
             autoComplete="email"
             autoFocus
+            onChange={(event) => setEmail(event.target.value)}
           />
           <TextField
             variant="outlined"
@@ -46,6 +85,7 @@ export default function SignUpForm() {
             type="password"
             id="password"
             autoComplete="current-password"
+            onChange={(event) => setPassword(event.target.value)}
           />
           <Button
             type="submit"
@@ -56,6 +96,7 @@ export default function SignUpForm() {
           >
             Register
           </Button>
+          {errorAlert && displayError(messageError)}
           <Grid container>
             <Grid item xs>
               <Link href="#" variant="body2">
@@ -63,8 +104,8 @@ export default function SignUpForm() {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="sign-in" variant="body2">
-                {"Already have an account? Sign In"}
+              <Link href="/sign-in" variant="body2">
+                {"Already have an account? Login!"}
               </Link>
             </Grid>
           </Grid>
