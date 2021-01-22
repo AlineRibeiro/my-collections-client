18470670544ee545a8956a050816c4ec8b2f1906
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-21 16:33:36 -0800

    Extract navBar styles to separated file

--- src/features/shared/NavBar.jsx
+++ src/features/shared/NavBar.jsx
@@ -5,19 +5,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import { NavBarStyles } from './NavBarStyles';
 
 const NavBar = () => {
   // const [snackIndex, setSnackIndex] = useState("");
@@ -31,7 +20,7 @@ const NavBar = () => {
   // useEffect(() => {
   //   fetchSnackIndex();
   // });
-  const classes = useStyles();
+  const classes = NavBarStyles();
 
   return(
     <AppBar position="static">
@@ -49,10 +38,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
-
-
-
-
-
-
