d0c4be5ae5c5fb9a3a1bb1661d930c6e462d84f5
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-26 15:12:24 -0800

    Remove unused server mock

--- src/features/shared/NavBar.test.js
+++ src/features/shared/NavBar.test.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { Provider } from "react-redux";
@@ -8,18 +6,6 @@ import configureStore from "redux-mock-store";
 import { BrowserRouter } from "react-router-dom";
 
 import NavBar from "./NavBar";
-
-const server = setupServer(
-  rest.get("/current_user", (req, res, ctx) => {
-    return res(
-      ctx.json({ error: "You need to sign in or sign up before continuing." })
-    );
-  })
-);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
 
 test("displays a login button", async () => {
   const initialState = { user: {} };
