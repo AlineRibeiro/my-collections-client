3250028b62a9629668492cd6f83247cec541de34
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-26 13:29:03 -0800

    Test if login button is displayed for guest user

---
+++ src/features/shared/NavBar.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { BrowserRouter } from "react-router-dom";
+
+import NavBar from "./NavBar";
+
+const server = setupServer(
+  rest.get("/current_user", (req, res, ctx) => {
+    return res(
+      ctx.json({ error: "You need to sign in or sign up before continuing." })
+    );
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test("displays a login button", async () => {
+  const initialState = { user: {} };
+  const mockStore = configureStore();
+  let store = mockStore(initialState);
+
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+    </Provider>
+  );
+
+  await waitFor(() => screen.getByText("Login"));
+
+  expect(screen.getByText("Login")).toBeInTheDocument();
+  expect(screen.getByText("Register")).toBeInTheDocument();
+});
