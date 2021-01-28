1f6aac9b3452d61f8024ef60224b71d3fc4074ae
Aline Ribeiro <alinear.mail@gmail.com>
2021-01-28 15:07:29 -0800

    Wrap component in provider to fix test

--- src/features/snacks/SnackParent.test.js
+++ src/features/snacks/SnackParent.test.js
@@ -2,9 +2,12 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 
 import SnackParent from "./SnackParent";
+import {Provider} from "react-redux";
+import store from "../../app/store";
+
 
 test("renders learn react link", () => {
-  render(<SnackParent />);
-  const textElement = screen.getByText(/This is:/i);
+  render( <Provider store={store}><SnackParent /></Provider>);
+  const textElement = screen.getByText(/create a snack/i);
   expect(textElement).toBeInTheDocument();
 });
